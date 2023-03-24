317ec29e96438b4c12d924257f6a88e6925adb95
Ajay Sarkate <115990815+ajay-s-07@users.noreply.github.com>
2023-03-24 11:57:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+let age = document.querySelector("#age");
+let name = document.querySelector("#name");
+let form = document.querySelector("#form");
+// let button = document.querySelector("#btn").value;
+
+// function abc(event){
+	
+// }
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	
+	let age2 = parseInt(age.value);
+	const promise = new Promise((res, rej) => {
+		setTimeout(() => {
+			if(age >= 18){
+				resolve();
+			}
+			else reject();
+		}, 4000);
+	});
+	promise.then(() => {
+		alert(`Welcome, '${name.value}'. You can vote.`);
+	})
+	.catch(() => {
+		alert(`Oh sorry, '${name.value}'. You aren't old enough.`);
+	});
+});
+
+
+
+
+
+
+
+
